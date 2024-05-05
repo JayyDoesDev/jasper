8b8922e20288328e264a7709ea261023152c4457
Proman4713 <85965528+Proman4713@users.noreply.github.com>
2024-05-05 20:01:04 +0300

    Update TagsPlugin.ts

--- Plugins/Tags/TagsPlugin.ts
+++ Plugins/Tags/TagsPlugin.ts
@@ -4,13 +4,7 @@ import { TagCommand, TagCreateModal, TagEditModal, TagEvent, ResolvedCommand } f
 export = DefinePlugin({
   name: "tags",
   description: "Tags for the No Text To Speech support team!",
-  commands: [TagCommand],
+  commands: [TagCommand, ResolvedCommand],
   events: [TagCreateModal, TagEditModal, TagEvent],
-  public_plugin: true
-}, {
-  name: "resolved",
-  description: "Marks post as resolved and sends a message to inform OP",
-  commands: [ResolvedCommand],
-  events: [],
   public_plugin: true
 }) as Plugin;
