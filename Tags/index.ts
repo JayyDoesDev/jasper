4680f7b3c0a786ca47cf16074d92c31f5c793be8
Proman4713 <85965528+Proman4713@users.noreply.github.com>
2024-05-05 14:32:41 +0300

    Added ResolvedCommand to index.ts

--- Tags/index.ts
+++ Tags/index.ts
@@ -1,4 +1,5 @@
 export * from "./Commands/TagCommand";
+export * from "./Commands/ResolvedCommand";
 export * from "./Modals/TagCreateModal";
 export * from "./Modals/TagEditModal";
 export * from "./Events/TagEvent";
