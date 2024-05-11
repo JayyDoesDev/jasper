df65929d2136e758e9b6a0ae0b5e10ea4c872c36
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2024-05-10 23:11:43 -0400

    feat: Status

--- Javascript/CoreCommands.js
+++ Javascript/CoreCommands.js
@@ -13,6 +13,6 @@ module.exports = {
         content: "pong!",
         ephemeral: true
       });
-    };
-  });
+    }
+  })
 };
--- Plugins/Core/CorePlugin.ts
+++ Plugins/Core/CorePlugin.ts
@@ -1,15 +1,15 @@
 import { DefinePlugin, Plugin } from "../../Common/DefinePlugin";
 import { DefineEvent } from "../../Common/DefineEvent";
-import { ChannelType, ChatInputCommandInteraction, Interaction } from "discord.js";
+import { ActivityType, ChannelType, ChatInputCommandInteraction, Interaction } from "discord.js";
 import { Command, DefineCommand } from "../../Common/DefineCommand";
 import { ApplicationCommandType, PermissionsToHuman, PlantPermission, Permissions, PermissionBitToString } from "@antibot/interactions";
 import { PingCommand } from "../../Javascript/CoreCommands";
 import numeral from "numeral";
 import { Context } from "../../Context";
 export = DefinePlugin({
-	name: "Core",
-	description: "Core process.",
-	commands: [
+  name: "Core",
+  description: "Core process.",
+  commands: [
     DefineCommand({
       command: {
         name: "secret",
@@ -33,65 +33,74 @@ export = DefinePlugin({
     }),
     PingCommand
   ],
-	events: [
-		DefineEvent({
-			event: {
-				name: "ready",
-				once: true,
-			},
-			on: (ctx) => {
+  events: [
+    DefineEvent({
+      event: {
+        name: "ready",
+        once: true,
+      },
+      on: (ctx: Context) => {
         if (process.env.SUB_COUNT_UPDATE == "1") {
           (async () => {
             const data = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${process.env.YOUTUBE_CHANNEL_ID}&key=${process.env.YOUTUBE_KEY}`);
             data.json().then((x) => {
-                const subscriberCount: string = numeral(x.items[0].statistics.subscriberCount).format('0.00a');
+              const subscriberCount: string = numeral(x.items[0].statistics.subscriberCount).format('0.00a');
               ctx.channels.cache.get(process.env.SUB_COUNT_CHANNEL)
                 //@ts-ignore
                 .setName(`\u{1F4FA} \u{FF5C} Sub Count: ${subscriberCount}`);
             });
           })()
         }
-				console.log(`${ctx.user.username} has logged in!`);
-			},
-		}),
-		DefineEvent({
-			event: {
-				name: "interactionCreate",
-				once: false,
-			},
-			on: (interaction, ctx) => {
-				if (!interaction.isCommand()) {
-					return;
-				}
-				const command: Command = ctx.interactions.get(interaction.commandName);
-				if (command) {
-					if (command.permissions) {
-						const perms: any[] = [];
-						let permDisplay: string = "";
-						if (!interaction.appPermissions.has(command.permissions)) {
-							//@ts-ignore
-							for (let i = 0; i < command.permissions.length; i++) {
-								perms.push(
-									PermissionsToHuman(PlantPermission(command.permissions[i]))
-								);
-							}
-							if (perms.length <= 2) {
-								permDisplay = perms.join(" & ");
-							} else {
-								permDisplay = perms.join(", ");
-							}
-							//@ts-ignore
-							return interaction.reply({
-								content: `I'm missing permissions! (${permDisplay})`,
+        console.log(`${ctx.user.username} has logged in!`);
+        ctx.user.setPresence({
+          activities: [
+            {
+              type: ActivityType.Custom,
+              name: "custom",
+              state: "jasper"
+            }
+          ]
+        });
+      },
+    }),
+    DefineEvent({
+      event: {
+        name: "interactionCreate",
+        once: false,
+      },
+      on: (interaction, ctx) => {
+        if (!interaction.isCommand()) {
+          return;
+        }
+        const command: Command = ctx.interactions.get(interaction.commandName);
+        if (command) {
+          if (command.permissions) {
+            const perms: any[] = [];
+            let permDisplay: string = "";
+            if (!interaction.appPermissions.has(command.permissions)) {
+              //@ts-ignore
+              for (let i = 0; i < command.permissions.length; i++) {
+                perms.push(
+                  PermissionsToHuman(PlantPermission(command.permissions[i]))
+                );
+              }
+              if (perms.length <= 2) {
+                permDisplay = perms.join(" & ");
+              } else {
+                permDisplay = perms.join(", ");
+              }
+              //@ts-ignore
+              return interaction.reply({
+                content: `I'm missing permissions! (${permDisplay})`,
                 ephemeral: true
-							});
-						}
-					}
+              });
+            }
+          }
 
-					command.on(ctx, interaction);
-				}
-			},
-		}),
+          command.on(ctx, interaction);
+        }
+      },
+    }),
     DefineEvent({
       event: {
         name: "error",
@@ -101,6 +110,6 @@ export = DefinePlugin({
         console.log(`Error: ${e.rawError.message}\nMethod: ${e.method}\nUrl: ${e.url} `);
       }
     })
-	],
-	public_plugin: true
+  ],
+  public_plugin: true
 }) as Plugin;
