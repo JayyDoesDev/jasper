97e3b45282cd19fc196f6ed83662d6f3316c3c6d
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2024-05-27 11:10:03 -0400

    feat: bool variable removed and returns result in one line instead

--- Common/GuildExists.ts
+++ Common/GuildExists.ts
@@ -2,7 +2,5 @@ import GuildSchema from "../Models/TagSchema";
 import type { Snowflake } from "@antibot/interactions";
 
 export async function GuildExists(guildId: Snowflake): Promise<boolean> {
-    let bool: boolean;
-    await GuildSchema.findOne({ _id: guildId }) ? bool = true : bool = false;
-    return bool;
+  return await GuildSchema.findOne({ _id: guildId }) ? true : false;
 }
