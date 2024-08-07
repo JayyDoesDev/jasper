5a3c037f8639294fb8436579b8642b57a9b2bbc7
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2024-08-06 22:02:22 -0400

    chore: Switched to an oop structure

--- Plugins/Tags/SubCommands/CreateSubCommand.ts
+++ Plugins/Tags/SubCommands/CreateSubCommand.ts
@@ -1,58 +1,86 @@
 import { ApplicationCommandOptions, ApplicationCommandOptionType } from "@antibot/interactions";
 import { Context } from "../../../Source/Context";
-import { ActionRowBuilder, ChatInputCommandInteraction, ModalBuilder, TextInputBuilder, TextInputStyle } from "discord.js";
+import { ChatInputCommandInteraction, ComponentType, TextInputStyle } from "discord.js";
 import { RegisterSubCommand } from "../../../Common/RegisterSubCommand";
 
 export const CreateSubCommand: ApplicationCommandOptions = {
-    name: "create",
-    description: "Create a tag!",
-    type: ApplicationCommandOptionType.SUB_COMMAND,
-    options: []
+  name: "create",
+  description: "Create a tag!",
+  type: ApplicationCommandOptionType.SUB_COMMAND,
+  options: []
 } as ApplicationCommandOptions;
 
 export async function RunCreateSubCommand(ctx: Context, interaction: ChatInputCommandInteraction) {
-    await RegisterSubCommand({
-        subCommand: "create",
-        ctx: ctx,
-        interaction: interaction,
-        callback: async (ctx: Context, interaction: ChatInputCommandInteraction) => {
-            const modal: ModalBuilder = new ModalBuilder()
-                .setCustomId(`tag_create_${ interaction.user.id }`)
-                .setTitle("Support Tag Create");
-            const tagEmbedName: TextInputBuilder = new TextInputBuilder()
-                .setCustomId("tag_create_embed_name")
-                .setLabel("Tag")
-                .setPlaceholder("support")
-                .setMaxLength(80)
-                .setStyle(TextInputStyle.Short)
-                .setRequired(true);
-            const tagEmbedTitle: TextInputBuilder = new TextInputBuilder()
-                .setCustomId("tag_create_embed_title")
-                .setLabel("Embed Title")
-                .setPlaceholder("How do I contact support?")
-                .setMaxLength(200)
-                .setStyle(TextInputStyle.Short)
-                .setRequired(true);
-            const TagEmbedDescription: TextInputBuilder = new TextInputBuilder()
-                .setCustomId("tag_create_embed_description")
-                .setLabel("Embed Description")
-                .setPlaceholder("You can contact us in the support threads!")
-                .setMaxLength(3000)
-                .setStyle(TextInputStyle.Paragraph)
-                .setRequired(false);
-            const TagEmbedFooter: TextInputBuilder = new TextInputBuilder()
-                .setCustomId("tag_create_embed_footer")
-                .setLabel("Embed Footer")
-                .setPlaceholder("Make sure to be patient!")
-                .setMaxLength(40)
-                .setStyle(TextInputStyle.Short)
-                .setRequired(false);
-            const tagEmbedNameRow: ActionRowBuilder<TextInputBuilder> = new ActionRowBuilder<TextInputBuilder>().addComponents(tagEmbedName);
-            const tagEmbedTitleRow: ActionRowBuilder<TextInputBuilder> = new ActionRowBuilder<TextInputBuilder>().addComponents(tagEmbedTitle);
-            const TagEmbedDescriptionRow: ActionRowBuilder<TextInputBuilder> = new ActionRowBuilder<TextInputBuilder>().addComponents(TagEmbedDescription);
-            const tagEmbedFooterRow: ActionRowBuilder<TextInputBuilder> = new ActionRowBuilder<TextInputBuilder>().addComponents(TagEmbedFooter);
-            modal.addComponents(tagEmbedNameRow, tagEmbedTitleRow, TagEmbedDescriptionRow, tagEmbedFooterRow);
-            await interaction.showModal(modal);
+  await RegisterSubCommand({
+    subCommand: "create",
+    ctx: ctx,
+    interaction: interaction,
+    callback: async (ctx: Context, interaction: ChatInputCommandInteraction) => {
+
+      await interaction.showModal(
+        {
+          customId: `tag_create_${interaction.user.id}`,
+          title: "Create a support tag",
+          components: [
+            {
+              type: ComponentType.ActionRow,
+              components: [
+                {
+                  type: ComponentType.TextInput,
+                  customId: "tag_create_embed_name",
+                  label: "Tag",
+                  placeholder: "support",
+                  maxLength: 80,
+                  style: TextInputStyle.Short,
+                  required: true
+                }
+              ]
+            },
+            {
+              type: ComponentType.ActionRow,
+              components: [
+                {
+                  type: ComponentType.TextInput,
+                  customId: "tag_create_embed_title",
+                  label: "Embed Title",
+                  placeholder: "How do I contact support?",
+                  maxLength: 200,
+                  style: TextInputStyle.Short,
+                  required: true
+                }
+              ]
+            },
+            {
+              type: ComponentType.ActionRow,
+              components: [
+                {
+                  type: ComponentType.TextInput,
+                  customId: "tag_create_embed_description",
+                  label: "Embed Description",
+                  placeholder: "You can contact us in the support threads!",
+                  maxLength: 3000,
+                  style: TextInputStyle.Paragraph,
+                  required: false
+                }
+              ]
+            },
+            {
+              type: ComponentType.ActionRow,
+              components: [
+                {
+                  type: ComponentType.TextInput,
+                  customId: "tag_create_embed_footer",
+                  label: "Embed Footer",
+                  placeholder: "Make sure to be patient!",
+                  maxLength: 40,
+                  style: TextInputStyle.Short,
+                  required: false
+                }
+              ]
+            }
+          ]
         }
-    });
+      );
+    }
+  });
 }
--- Plugins/Tags/SubCommands/EditSubCommand.ts
+++ Plugins/Tags/SubCommands/EditSubCommand.ts
@@ -1,6 +1,6 @@
 import { ApplicationCommandOptions, ApplicationCommandOptionType } from "@antibot/interactions";
 import { Context } from "../../../Source/Context";
-import { ActionRowBuilder, ChatInputCommandInteraction, ModalBuilder, TextInputBuilder, TextInputStyle } from "discord.js";
+import { ChatInputCommandInteraction, ComponentType, TextInputStyle } from "discord.js";
 import { RegisterSubCommand } from "../../../Common/RegisterSubCommand";
 
 export const EditSubCommand: ApplicationCommandOptions = {
@@ -16,43 +16,70 @@ export async function RunEditSubCommand(ctx: Context, interaction: ChatInputComm
         ctx: ctx,
         interaction: interaction,
         callback: async (ctx: Context, interaction: ChatInputCommandInteraction) => {
-            const modal: ModalBuilder = new ModalBuilder()
-                .setCustomId(`tag_edit_${ interaction.user.id }`)
-                .setTitle("Support Tag Edit")
-            const tagEmbedName: TextInputBuilder = new TextInputBuilder()
-                .setCustomId("tag_edit_embed_name")
-                .setLabel("Tag")
-                .setPlaceholder("support")
-                .setMaxLength(80)
-                .setStyle(TextInputStyle.Short)
-                .setRequired(true);
-            const tagEmbedTitle: TextInputBuilder = new TextInputBuilder()
-                .setCustomId("tag_edit_embed_title")
-                .setLabel("Embed Title")
-                .setPlaceholder("How do I contact support?")
-                .setMaxLength(200)
-                .setStyle(TextInputStyle.Short)
-                .setRequired(false);
-            const TagEmbedDescription: TextInputBuilder = new TextInputBuilder()
-                .setCustomId("tag_edit_embed_description")
-                .setLabel("Embed Description")
-                .setPlaceholder("You can contact us in the support threads!")
-                .setMaxLength(3000)
-                .setStyle(TextInputStyle.Paragraph)
-                .setRequired(false);
-            const TagEmbedFooter: TextInputBuilder = new TextInputBuilder()
-                .setCustomId("tag_edit_embed_footer")
-                .setLabel("Embed Footer")
-                .setPlaceholder("Make sure to be patient!")
-                .setMaxLength(40)
-                .setStyle(TextInputStyle.Short)
-                .setRequired(false);
-            const tagEmbedNameRow: ActionRowBuilder<TextInputBuilder> = new ActionRowBuilder<TextInputBuilder>().addComponents(tagEmbedName);
-            const tagEmbedTitleRow: ActionRowBuilder<TextInputBuilder> = new ActionRowBuilder<TextInputBuilder>().addComponents(tagEmbedTitle);
-            const TagEmbedDescriptionRow: ActionRowBuilder<TextInputBuilder> = new ActionRowBuilder<TextInputBuilder>().addComponents(TagEmbedDescription);
-            const tagEmbedFooterRow: ActionRowBuilder<TextInputBuilder> = new ActionRowBuilder<TextInputBuilder>().addComponents(TagEmbedFooter);
-            modal.addComponents(tagEmbedNameRow, tagEmbedTitleRow, TagEmbedDescriptionRow, tagEmbedFooterRow);
-            await interaction.showModal(modal);
+            await interaction.showModal(
+              {
+                customId: `tag_edit_${ interaction.user.id }`,
+                title: "Edit a support tag",
+                components: [
+                  {
+                    type: ComponentType.ActionRow,
+                    components: [
+                      {
+                        type: ComponentType.TextInput,
+                        customId: "tag_edit_embed_name",
+                        label: "Tag",
+                        placeholder: "support",
+                        maxLength: 80,
+                        style: TextInputStyle.Short,
+                        required: true
+                      }
+                    ]
+                  },
+                  {
+                    type: ComponentType.ActionRow,
+                    components: [
+                      {
+                        type: ComponentType.TextInput,
+                        customId: "tag_edit_embed_title",
+                        label: "Embed Title",
+                        placeholder: "How do i contact support?",
+                        maxLength: 200,
+                        style: TextInputStyle.Short,
+                        required: false
+                      }
+                    ]
+                  },
+                  {
+                    type: ComponentType.ActionRow,
+                    components: [
+                      {
+                        type: ComponentType.TextInput,
+                        customId: "tag_edit_embed_description",
+                        label: "Embed Description",
+                        placeholder: "You can contact us in the support threads!",
+                        maxLength: 3000,
+                        style: TextInputStyle.Paragraph,
+                        required: false
+                      }
+                    ]
+                  },
+                  {
+                    type: ComponentType.ActionRow,
+                    components: [
+                      {
+                        type: ComponentType.TextInput,
+                        customId: "tag_edit_embed_footer",
+                        label: "Embed Footer",
+                        placeholder: "Make sure to be patient!",
+                        maxLength: 40,
+                        style: TextInputStyle.Short,
+                        required: false
+                      }
+                    ]
+                  }
+                ]
+              }
+            )
         }
     })
 }
