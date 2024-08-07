b0a4eebfbbbf925e36778195dea5ea9f3af9d6df
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2024-08-06 20:52:18 -0400

    Update TagSchema.ts

--- Models/TagSchema.ts
+++ Models/TagSchema.ts
@@ -2,23 +2,21 @@ import { model, Schema } from "mongoose";
 import { TagDocument } from "./TagDocument";
 import { MO } from "./MongoObject";
 
-const TagSchema: Schema = new Schema(
-    {
-        _id: String,
-        Tags: {
-            type: [
-                {
-                    TagName: String,
-                    TagAuthor: String,
-                    TagResponse: {
-                        TagEmbedTitle: MO(String, undefined, true),
-                        TagEmbedDescription: MO(String, null, false),
-                        TagEmbedFooter: MO(String, null, false)
-                    }
-                }
-            ],
-            default: []
-        },
-    }, { versionKey: false, timestamps: true });
-
-export default model<TagDocument>("support-tags", TagSchema);
+export default model<TagDocument>("support-tags", new Schema(
+  {
+      _id: String,
+      Tags: {
+          type: [
+              {
+                  TagName: String,
+                  TagAuthor: String,
+                  TagResponse: {
+                      TagEmbedTitle: MO(String, undefined, true),
+                      TagEmbedDescription: MO(String, null, false),
+                      TagEmbedFooter: MO(String, null, false)
+                  }
+              }
+          ],
+          default: []
+      },
+  }, { versionKey: false, timestamps: true }));
