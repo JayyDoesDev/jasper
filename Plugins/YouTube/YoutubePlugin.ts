2f7386fcc12fe022b006bd238b5fe3ec16fde95e
Tarab1te <69683068+JayyDoesDev@users.noreply.github.com>
2024-08-28 13:39:57 -0400

    Update YoutubePlugin.ts

--- Plugins/YouTube/YoutubePlugin.ts
+++ Plugins/YouTube/YoutubePlugin.ts
@@ -1,1 +1,12 @@
-keep
+import { DefinePlugin, Plugin } from "../../Common/DefinePlugin";
+import { MassRegisterCommands } from "../../Common/MassRegisterCommands";
+import { MassRegisterEvents } from "../../Common/MassRegisterEvents";
+
+export = DefinePlugin({
+    name: "youtube",
+    description: "To do things like force notify about videos.",
+    commands: MassRegisterCommands(__dirname, ["Commands"]),
+    events: MassRegisterEvents(__dirname, ["Events", "Modals", "Buttons"]),
+    public_plugin: true,
+}) as Plugin;
+
