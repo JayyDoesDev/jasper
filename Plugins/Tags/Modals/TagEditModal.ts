ef0a3431e9a3503b8e65c66a27659ac67c19a213
Tarab1te <69683068+JayyDoesDev@users.noreply.github.com>
2024-09-16 14:07:27 -0400

    chore: Update

--- Plugins/Tags/Modals/TagEditModal.ts
+++ Plugins/Tags/Modals/TagEditModal.ts
@@ -24,15 +24,11 @@ export = {
                 negativeTypeGuards: [ "isModalSubmit" ]
             },
             callback: async (ctx: Context, interaction: ModalSubmitInteraction) => {
-                const tagEmbedName: string = interaction.fields.getTextInputValue("tag_edit_embed_name");
-                const tagEmbedTitle: string = interaction.fields.getTextInputValue("tag_edit_embed_title");
-                const TagEmbedDescription: string = interaction.fields.getTextInputValue("tag_edit_embed_description");
-                const tagEmbedFooter: string = interaction.fields.getTextInputValue("tag_edit_embed_footer");
                 const tag: TagOptions = {
-                    name: tagEmbedName,
-                    title: tagEmbedTitle,
-                    description: TagEmbedDescription ? TagEmbedDescription : null,
-                    footer: tagEmbedFooter ? tagEmbedFooter : null
+                    name: interaction.fields.getTextInputValue("tag_edit_embed_name"),
+                    title: interaction.fields.getTextInputValue("tag_edit_embed_title"),
+                    description: interaction.fields.getTextInputValue("tag_edit_embed_description") ? interaction.fields.getTextInputValue("tag_edit_embed_description") : null,
+                    footer: interaction.fields.getTextInputValue("tag_edit_embed_footer") ? interaction.fields.getTextInputValue("tag_edit_embed_footer") : null
                 };
                 if (await TagExists({ guildId: interaction.guild.id, name: tag.name, ctx: ctx })) {
                     await TagEdit({ guildId: interaction.guild.id, options: { name: tag.name, title: tag.title, description: tag.description, footer: tag.footer }, ctx: ctx })
