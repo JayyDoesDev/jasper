f905cf60f3bb9fa2408f5081fcf3fadec7c8c4d0
Tarab1te <69683068+JayyDoesDev@users.noreply.github.com>
2024-09-22 16:15:26 -0400

    Update Store.ts

--- Source/Store.ts
+++ Source/Store.ts
@@ -17,27 +17,31 @@ export class Store extends Redis {
     }
 
     public async getUser<T>(options: UserSnowflake): Promise<T> {
-      return JSON.parse(await this.get(JSON.stringify(options)));
-  }
+        return JSON.parse(await this.get(JSON.stringify(options)));
+    }
 
     public async findGuild(options: GuildSnowflake): Promise<boolean> {
         return await this.getGuild(options) ? true : false;
     }
 
     public async findUser(options: UserSnowflake): Promise<boolean> {
-      return await this.getUser(options) ? true : false;
+        return await this.getUser(options) ? true : false;
     }
 
-    public deleteGuild(options: GuildSnowflake | UserSnowflake): void{
+    public deleteGuild(options: GuildSnowflake | UserSnowflake): void {
         this.del(JSON.stringify(options));
     }
 
     public setKey<T>(options: GuildSnowflake | UserSnowflake, ...keys: T[] | []): void {
-         this.set(JSON.stringify(options), JSON.stringify(keys || []));
+        this.set(JSON.stringify(options), JSON.stringify(keys || []));
     }
 
     public async setUserKey<T>(options: UserSnowflake, data: T): Promise<void> {
-      await this.set(JSON.stringify(options), JSON.stringify(data));
+        await this.set(JSON.stringify(options), JSON.stringify(data));
+    }
+
+    public async setForeignKey<T>(options: Object | string, data: T): Promise<void> {
+        await this.set(JSON.stringify(options), JSON.stringify(data));
     }
 
     public guildExists(options: GuildSnowflake | UserSnowflake): Promise<number> {
