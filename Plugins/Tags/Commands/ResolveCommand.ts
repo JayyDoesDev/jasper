4c83d8387c7437db6f374deb676a825c09c78d42
GuikiPT <foxzillapt17@gmail.com>
2024-12-07 07:41:44 -0500

    [ADD] Add safeguard to prevent exceeding Discord's tag limit when applying "Resolved" tag

--- Plugins/Tags/Commands/ResolveCommand.ts
+++ Plugins/Tags/Commands/ResolveCommand.ts
@@ -5,82 +5,92 @@ import { ChannelType, ChatInputCommandInteraction } from "discord.js";
 import { CheckForRoles } from "../../../Common/CheckForRoles";
 
 export = {
-  Command: DefineCommand<ChatInputCommandInteraction>({
-    command: {
-      name: "resolve",
-      type: ApplicationCommandType.CHAT_INPUT,
-      description: "Marks post as resolved and sends a message to inform OP",
-      options: [
-          {
-              type: ApplicationCommandOptionType.STRING,
-              name: "original_question",
-              description: "Original Question asked by OP",
-              required: false,
-          },
-          {
-              type: ApplicationCommandOptionType.STRING,
-              name: "summarized_answer",
-              description:
-                  "Summarization of the answer to the OP's original question",
-              required: false,
-          },
-      ],
-  },
-  permissions: [ PermissionsBitField.ManageThreads ],
-  on: async (ctx, interaction) => {
-      if (CheckForRoles(interaction, ctx.env.get("admin"), ctx.env.get("staff"), ctx.env.get("support"))) {
-          const finalReply: Record<"content", string> = { content: `Post marked as Resolved by <@${ interaction.user.id }>`, };
-          const originalQuestion: string = interaction.options.getString("original_question");
-          const summarizedAnswer: string = interaction.options.getString("summarized_answer");
-          const embeds: { title: string, fields: { name: string, value: string }[], color: number }[] = [ { title: "Overview", fields: [], color: global.embedColor }];
-          
-          const appliedTags = (interaction.channel as any).appliedTags;
-          if (interaction.channel.type == ChannelType.PublicThread) {
-              if (!appliedTags.includes("1144008960966402149")) {
-                  const tagIndex = appliedTags.indexOf("1284733312501420145");
-                  if (tagIndex !== -1) {
-                      appliedTags.splice(tagIndex, 1);
-                  }
-                  await interaction.channel.setAppliedTags([
-                      "1144008960966402149",
-                      ...appliedTags,
-                  ]);
-              }
-              if (originalQuestion) {
-                  embeds[0].fields.push({
-                      name: "Original Question",
-                      value: originalQuestion,
-                  });
-              }
-              if (summarizedAnswer) {
-                  embeds[0].fields.push({
-                      name: "Summarized Answer",
-                      value: summarizedAnswer,
-                  });
-              }
-              if (embeds[0].fields.length > 0) {
-                  finalReply["embeds"] = embeds;
-              }
-              await interaction.reply(finalReply);
-              if (!interaction.channel.locked) {
-                  await interaction.channel.setLocked(true);
-              }
-              if (!interaction.channel.archived) {
-                  await interaction.channel.setArchived(true);
-              }
-              return;
-          } else {
-              return interaction.reply({
-                  content:
-                      "Channel is not a thread. This command **must be** executed in Forum Posts!",
-                  ephemeral: true,
-              });
-          }
-      }
-      return interaction.reply({
-          content: "Sorry but you can't use this command.",
-          ephemeral: true,
-      });
-    },
-  })
+    Command: DefineCommand<ChatInputCommandInteraction>({
+        command: {
+            name: "resolve",
+            type: ApplicationCommandType.CHAT_INPUT,
+            description: "Marks post as resolved and sends a message to inform OP",
+            options: [
+                {
+                    type: ApplicationCommandOptionType.STRING,
+                    name: "original_question",
+                    description: "Original Question asked by OP",
+                    required: false,
+                },
+                {
+                    type: ApplicationCommandOptionType.STRING,
+                    name: "summarized_answer",
+                    description:
+                        "Summarization of the answer to the OP's original question",
+                    required: false,
+                },
+            ],
+        },
+        permissions: [PermissionsBitField.ManageThreads],
+        on: async (ctx, interaction) => {
+            if (CheckForRoles(interaction, ctx.env.get("admin"), ctx.env.get("staff"), ctx.env.get("support"))) {
+                const finalReply: Record<"content", string> = { content: `Post marked as Resolved by <@${interaction.user.id}>`, };
+                const originalQuestion: string = interaction.options.getString("original_question");
+                const summarizedAnswer: string = interaction.options.getString("summarized_answer");
+                const embeds: { title: string, fields: { name: string, value: string }[], color: number }[] = [{ title: "Overview", fields: [], color: global.embedColor }];
+
+                const appliedTags = (interaction.channel as any).appliedTags;
+                if (interaction.channel.type == ChannelType.PublicThread) {
+                    if (appliedTags.length >= 5) {
+                        return interaction.reply({
+                            content:
+                                "This thread already has 5 tags applied.\nPlease remove one tag before executing `/resolve` again!",
+                            ephemeral: true,
+                        });
+                    }
+
+                    // TODO: Use Enviroment Variables to specify the "Resolved" and "Waiting for Update" tag ids
+                    // TODO: Instead hardcoding the ids.
+                    if (!appliedTags.includes("1144008960966402149")) {
+                        const tagIndex = appliedTags.indexOf("1284733312501420145");
+                        if (tagIndex !== -1) {
+                            appliedTags.splice(tagIndex, 1);
+                        }
+                        await interaction.channel.setAppliedTags([
+                            "1144008960966402149",
+                            ...appliedTags,
+                        ]);
+                    }
+                    if (originalQuestion) {
+                        embeds[0].fields.push({
+                            name: "Original Question",
+                            value: originalQuestion,
+                        });
+                    }
+                    if (summarizedAnswer) {
+                        embeds[0].fields.push({
+                            name: "Summarized Answer",
+                            value: summarizedAnswer,
+                        });
+                    }
+                    if (embeds[0].fields.length > 0) {
+                        finalReply["embeds"] = embeds;
+                    }
+                    await interaction.reply(finalReply);
+                    if (!interaction.channel.locked) {
+                        await interaction.channel.setLocked(true);
+                    }
+                    if (!interaction.channel.archived) {
+                        await interaction.channel.setArchived(true);
+                    }
+                    return;
+                } else {
+                    return interaction.reply({
+                        content:
+                            "Channel is not a thread. This command **must be** executed in Forum Posts!",
+                        ephemeral: true,
+                    });
+                }
+            }
+            return interaction.reply({
+                content: "Sorry but you can't use this command.",
+                ephemeral: true,
+            });
+        },
+    })
 }
