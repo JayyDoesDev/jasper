8ceb4c11685e649cedde2be57941dd5672b3f8e3
Tarab1te <69683068+JayyDoesDev@users.noreply.github.com>
2025-01-01 01:56:50 -0500

    chore: Slowly switching over to TagController

--- Plugins/Tags/SubCommands/CreateSubCommand.ts
+++ Plugins/Tags/SubCommands/CreateSubCommand.ts
@@ -1,7 +1,6 @@
 import { ApplicationCommandOptions, ApplicationCommandOptionType } from "@antibot/interactions";
 import { Context } from "../../../Source/Context";
 import { ChatInputCommandInteraction, ComponentType, TextInputStyle } from "discord.js";
-import { RegisterSubCommand } from "../../../Common/RegisterSubCommand";
 
 export const CreateSubCommand: ApplicationCommandOptions = {
   name: "create",
@@ -11,90 +10,84 @@ export const CreateSubCommand: ApplicationCommandOptions = {
 } as ApplicationCommandOptions;
 
 export async function RunCreateSubCommand(ctx: Context, interaction: ChatInputCommandInteraction) {
-  await RegisterSubCommand({
-    subCommand: "create",
-    ctx: ctx,
-    interaction: interaction,
-    callback: async (ctx: Context, interaction: ChatInputCommandInteraction) => {
-
-      await interaction.showModal(
-        {
-          customId: `tag_create_${interaction.user.id}`,
-          title: "Create a support tag",
-          components: [
-            {
-              type: ComponentType.ActionRow,
-              components: [
-                {
-                  type: ComponentType.TextInput,
-                  customId: "tag_create_embed_name",
-                  label: "Tag",
-                  placeholder: "support",
-                  maxLength: 80,
-                  style: TextInputStyle.Short,
-                  required: true
-                }
-              ]
-            },
-            {
-              type: ComponentType.ActionRow,
-              components: [
-                {
-                  type: ComponentType.TextInput,
-                  customId: "tag_create_embed_title",
-                  label: "Embed Title",
-                  placeholder: "How do I contact support?",
-                  maxLength: 200,
-                  style: TextInputStyle.Short,
-                  required: true
-                }
-              ]
-            },
-            {
-              type: ComponentType.ActionRow,
-              components: [
-                {
-                  type: ComponentType.TextInput,
-                  customId: "tag_create_embed_description",
-                  label: "Embed Description",
-                  placeholder: "You can contact us in the support threads!",
-                  maxLength: 3000,
-                  style: TextInputStyle.Paragraph,
-                  required: false
-                }
-              ]
-            },
-            {
-              type: ComponentType.ActionRow,
-              components: [
-                {
-                  type: ComponentType.TextInput,
-                  customId: "tag_create_embed_image_url",
-                  label: "Embed Image URL",
-                  placeholder: "https://i.pinimg.com/originals/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg",
-                  maxLength: 500,
-                  style: TextInputStyle.Short,
-                  required: false
-                }
-              ]
-            },
-            {
-              type: ComponentType.ActionRow,
-              components: [
-                {
-                  type: ComponentType.TextInput,
-                  customId: "tag_create_embed_footer",
-                  label: "Embed Footer",
-                  placeholder: "Make sure to be patient!",
-                  maxLength: 40,
-                  style: TextInputStyle.Short,
-                  required: false
-                }
-              ]
-            }
-          ]
-        }
-      );
-    }
-  });
+  if (interaction.options.getSubcommand() === CreateSubCommand.name) {
+    await interaction.showModal(
+      {
+        customId: `tag_create_${interaction.user.id}`,
+        title: "Create a support tag",
+        components: [
+          {
+            type: ComponentType.ActionRow,
+            components: [
+              {
+                type: ComponentType.TextInput,
+                customId: "tag_create_embed_name",
+                label: "Tag",
+                placeholder: "support",
+                maxLength: 80,
+                style: TextInputStyle.Short,
+                required: true
+              }
+            ]
+          },
+          {
+            type: ComponentType.ActionRow,
+            components: [
+              {
+                type: ComponentType.TextInput,
+                customId: "tag_create_embed_title",
+                label: "Embed Title",
+                placeholder: "How do I contact support?",
+                maxLength: 200,
+                style: TextInputStyle.Short,
+                required: true
+              }
+            ]
+          },
+          {
+            type: ComponentType.ActionRow,
+            components: [
+              {
+                type: ComponentType.TextInput,
+                customId: "tag_create_embed_description",
+                label: "Embed Description",
+                placeholder: "You can contact us in the support threads!",
+                maxLength: 3000,
+                style: TextInputStyle.Paragraph,
+                required: false
+              }
+            ]
+          },
+          {
+            type: ComponentType.ActionRow,
+            components: [
+              {
+                type: ComponentType.TextInput,
+                customId: "tag_create_embed_image_url",
+                label: "Embed Image URL",
+                placeholder: "https://i.pinimg.com/originals/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg",
+                maxLength: 500,
+                style: TextInputStyle.Short,
+                required: false
+              }
+            ]
+          },
+          {
+            type: ComponentType.ActionRow,
+            components: [
+              {
+                type: ComponentType.TextInput,
+                customId: "tag_create_embed_footer",
+                label: "Embed Footer",
+                placeholder: "Make sure to be patient!",
+                maxLength: 40,
+                style: TextInputStyle.Short,
+                required: false
+              }
+            ]
+          }
+        ]
+      }
+    )
+  }
 }
--- Source/Context.ts
+++ Source/Context.ts
@@ -9,18 +9,22 @@ import { Env } from "./Env";
 import path from "path";
 import TagController from "../Controllers/TagController";
 
-type Controllers = {
-    tags: TagController;
+
+class Controllers {
+    public readonly tags: TagController;
+    constructor(public ctx: Context) {
+        this.tags = new TagController(ctx);
+    }
 }
 
 export class Context extends Client {
-    public plugin: ZillaCollection<string, Plugin>;
-    public interactions: ZillaCollection<string, Command<ChatInputCommandInteraction | ContextMenuCommandInteraction>>;
-    public interact: Interactions;
-    public pagination: ZillaCollection<Snowflake, State>;
-    public env: Env;
-    public store: Store;
-    public controllers: Controllers;
+    public readonly plugin!: ZillaCollection<string, Plugin>;
+    public readonly interactions!: ZillaCollection<string, Command<ChatInputCommandInteraction | ContextMenuCommandInteraction>>;
+    public readonly interact!: Interactions;
+    public readonly pagination!: ZillaCollection<Snowflake, State>;
+    public readonly env!: Env;
+    public readonly store!: Store;
+    public readonly controllers!: Controllers;
 
     constructor() {
         super({
@@ -37,7 +41,7 @@ export class Context extends Client {
                 Partials.User,
             ],
             allowedMentions: {
-                parse: [ "users" ],
+                parse: ["users"],
             },
             presence: {
                 activities: [
@@ -60,39 +64,39 @@ export class Context extends Client {
         });
         this.pagination = new ZillaCollection<Snowflake, State>();
         this.env = new Env(
-          { env: "BOTID", aliases: ["bot_id", "botid"] },
-          { env: "PUBLICKEY", aliases: ["public_key", "publickey"] },
-          { env: "TOKEN", aliases: ["botToken", "token", "bot"] },
-          { env: "MONGODB", aliases: ["db"] },
-          { env: "PREFIX", aliases: ["prefix"] },
-          { env: "SUPPORT_ROLE", aliases: ["support_role", "supportrole", "support"] },
-          { env: "ADMIN_ROLE", aliases: ["admin_role", "adminrole", "admin"] },
-          { env: "STAFF_ROLE", aliases: ["staff_role", "staffrole", "staff"] },
-          { env: "GUILD_ONLY_COMMANDS", aliases: ["guild_only_commands", "guild_only"] },
-          { env: "GUILD_ONLY_COMMANDS_GUILD_ID", aliases: ["guild_only_commands_guild_id", "guild_only_commands_id"] },
-          { env: "YOUTUBE_CHANNEL_ID", aliases: ["youtube_id", "youtube_channel_id", "youtube"]},
-          { env: "YOUTUBE_KEY", aliases: ["youtube_key_regular", "youtube_key_one"] },
-          { env: "YOUTUBE_KEY_TWO", aliases: ["youtube_key_two"] },
-          { env: "YOUTUBE_KEY_THREE", aliases: ["youtube_key_three"] },
-          { env: "YOUTUBE_VIDEO_POST_CHANNEL_ID", aliases: ["youtube_video_post_channel_id", "youtube_post_channel", "youtube_video_post_channel"] },
-          { env: "YOUTUBE_VIDEO_POST_TIMER", aliases: ["youtube_video_post_timer", "youtube_post_timer"] },
-          { env: "YOUTUBE_VIDEO_POST_UPDATE", aliases: ["youtube_video_post_update", "youtube_post_update"] },
-          { env: "YOUTUBE_VIDEO_DISCUSSIONS_ROLE_ID", aliases: ["youtube_video_discussions_role_id", "youtube_video_discussions_role", "youtube_video_disscussions"] },
-          { env: "SUB_COUNT_CHANNEL", aliases: ["sub_count_channel"] },
-          { env: "SUB_COUNT_TIMER", aliases: ["sub_count_time", "sub_timer", "sub_count_time"] },
-          { env: "SUB_COUNT_UPDATE", aliases: ["sub_count_update", "sub_update"] },
-          { env: "REDISHOST", aliases: ["redis_host", "redishost"] },
-          { env: "REDISPORT", aliases: ["redis_port", "redisport"] },
-          { env: "SUPPORT_THREAD", aliases: ["support_thread", "supporthread"] },
-          { env: "SLOWMODE", aliases: ["slowmode"] },
-          { env: "SLOWMODE_CHANNEL_ID", aliases: ["slowmode_channel"] },
-          { env: "SLOWMODE_COOLDOWN", aliases: ["slowmode_cooldown"] },
-          { env: "SLOWMODE_MESSAGE_TIME", aliases: ["slowmode_msg_time", "slowmode_message_time"] },
-          { env: "SLOWMODE_MESSAGE_THRESHOLD", aliases: ["slowmode_msg_threshold"] },
-          { env: "SLOWMODE_RESET_SLOWMODE", aliases: ["slowmode_reset_slowmode"] },
-          { env: "SLOWMODE_RESET_TIME", aliases: ["slowmode_reset_time"] }
-      );
-      this.store = new Store(this);
-      this.controllers = { tags: new TagController(this) }
+            { env: "BOTID", aliases: ["bot_id", "botid"] },
+            { env: "PUBLICKEY", aliases: ["public_key", "publickey"] },
+            { env: "TOKEN", aliases: ["botToken", "token", "bot"] },
+            { env: "MONGODB", aliases: ["db"] },
+            { env: "PREFIX", aliases: ["prefix"] },
+            { env: "SUPPORT_ROLE", aliases: ["support_role", "supportrole", "support"] },
+            { env: "ADMIN_ROLE", aliases: ["admin_role", "adminrole", "admin"] },
+            { env: "STAFF_ROLE", aliases: ["staff_role", "staffrole", "staff"] },
+            { env: "GUILD_ONLY_COMMANDS", aliases: ["guild_only_commands", "guild_only"] },
+            { env: "GUILD_ONLY_COMMANDS_GUILD_ID", aliases: ["guild_only_commands_guild_id", "guild_only_commands_id"] },
+            { env: "YOUTUBE_CHANNEL_ID", aliases: ["youtube_id", "youtube_channel_id", "youtube"] },
+            { env: "YOUTUBE_KEY", aliases: ["youtube_key_regular", "youtube_key_one"] },
+            { env: "YOUTUBE_KEY_TWO", aliases: ["youtube_key_two"] },
+            { env: "YOUTUBE_KEY_THREE", aliases: ["youtube_key_three"] },
+            { env: "YOUTUBE_VIDEO_POST_CHANNEL_ID", aliases: ["youtube_video_post_channel_id", "youtube_post_channel", "youtube_video_post_channel"] },
+            { env: "YOUTUBE_VIDEO_POST_TIMER", aliases: ["youtube_video_post_timer", "youtube_post_timer"] },
+            { env: "YOUTUBE_VIDEO_POST_UPDATE", aliases: ["youtube_video_post_update", "youtube_post_update"] },
+            { env: "YOUTUBE_VIDEO_DISCUSSIONS_ROLE_ID", aliases: ["youtube_video_discussions_role_id", "youtube_video_discussions_role", "youtube_video_disscussions"] },
+            { env: "SUB_COUNT_CHANNEL", aliases: ["sub_count_channel"] },
+            { env: "SUB_COUNT_TIMER", aliases: ["sub_count_time", "sub_timer", "sub_count_time"] },
+            { env: "SUB_COUNT_UPDATE", aliases: ["sub_count_update", "sub_update"] },
+            { env: "REDISHOST", aliases: ["redis_host", "redishost"] },
+            { env: "REDISPORT", aliases: ["redis_port", "redisport"] },
+            { env: "SUPPORT_THREAD", aliases: ["support_thread", "supporthread"] },
+            { env: "SLOWMODE", aliases: ["slowmode"] },
+            { env: "SLOWMODE_CHANNEL_ID", aliases: ["slowmode_channel"] },
+            { env: "SLOWMODE_COOLDOWN", aliases: ["slowmode_cooldown"] },
+            { env: "SLOWMODE_MESSAGE_TIME", aliases: ["slowmode_msg_time", "slowmode_message_time"] },
+            { env: "SLOWMODE_MESSAGE_THRESHOLD", aliases: ["slowmode_msg_threshold"] },
+            { env: "SLOWMODE_RESET_SLOWMODE", aliases: ["slowmode_reset_slowmode"] },
+            { env: "SLOWMODE_RESET_TIME", aliases: ["slowmode_reset_time"] }
+        );
+        this.store = new Store(this);
+        this.controllers = new Controllers(this);
     }
 }
