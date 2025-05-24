721fed07935a48748f276f949d56fa2a6223d42a
Tarab1te <69683068+JayyDoesDev@users.noreply.github.com>
2025-05-24 00:37:53 -0400

    cleanup

--- .webserver/src/main/kotlin/tara/tb1/pw/SecurityConfig.kt
+++
@@ -1,6 +0,0 @@
-package tara.tb1.pw.Security
-
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Configuration
-
-@Configuration class SecurityConfig(@Value("\${security.api-key}") val apiKey: String)
