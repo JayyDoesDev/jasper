825a86728a5c2060d48fd168c68cf1ca57761076
Tarab1te <69683068+JayyDoesDev@users.noreply.github.com>
2025-06-19 00:30:17 -0400

    chore: Env update for h2 db

--- apps/webserver/build.gradle.kts
+++ apps/webserver/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 }
 
 group = "com.example"
+
 version = "0.0.1-SNAPSHOT"
 
 java { toolchain { languageVersion = JavaLanguageVersion.of(21) } }
--- apps/webserver/src/main/kotlin/tara/tb1/pw/SecurityFilterConfig.kt
+++ apps/webserver/src/main/kotlin/tara/tb1/pw/SecurityFilterConfig.kt
@@ -10,7 +10,7 @@ class FilterConfig {
     @Bean
     fun apiKeyFilter(securityConfig: SecurityConfig): FilterRegistrationBean<ApiKeyFilter> {
         val registration = FilterRegistrationBean(ApiKeyFilter(securityConfig))
-        registration.addUrlPatterns("/playwright/*", "/web/*")
+        registration.addUrlPatterns("/playwright/*", "/web/*", "/fun/*")
         return registration
     }
 }
