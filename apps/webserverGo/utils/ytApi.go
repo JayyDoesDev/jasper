4754e068d21a2acaa3eb5d7adb8d6ca4fadce077
lumap <lumap@duck.com>
2025-06-30 23:54:44 +0200

    implement yt api key rotation

--- apps/webserverGo/utils/ytApi.go
+++ apps/webserverGo/utils/ytApi.go
@@ -1,12 +1,35 @@
 package utils
 
 import (
+	"math/rand"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 	"time"
 )
+
+func getApiKey() string {
+    apiKey1 := os.Getenv("YOUTUBE_API_KEY_1")
+    apiKey2 := os.Getenv("YOUTUBE_API_KEY_2")
+    apiKey3 := os.Getenv("YOUTUBE_API_KEY_3")
+
+    // return one of the three at random
+    apiKeys := []string{apiKey1, apiKey2, apiKey3}
+    var filtered []string
+	for _, str := range apiKeys {
+		if str != "" {
+			filtered = append(filtered, str)
+		}
+	}
+
+    if len(filtered) == 0 {
+        panic("No valid YouTube API keys found in environment variables")
+    }
+
+    randomIndex := rand.Intn(len(filtered))
+    return filtered[randomIndex]
+}
 
 func FetchChannelData(channelID string) (map[string]interface{}, error) {
 	CacheLock.RLock()
@@ -17,10 +40,9 @@ func FetchChannelData(channelID string) (map[string]interface{}, error) {
 		return cached.data, nil
 	}
 
-	apiKey := os.Getenv("YOUTUBE_API_KEY")
 	url := fmt.Sprintf(
 		"https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=%s&key=%s",
-		channelID, apiKey)
+		channelID, getApiKey())
 
 	resp, err := http.Get(url)
 	if err != nil {
