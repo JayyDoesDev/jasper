153842362487624f4c9ac584eef50cc5ab822bb4
Tarab1te <69683068+JayyDoesDev@users.noreply.github.com>
2025-07-01 17:35:17 -0400

    double equal

--- apps/bot/src/plugins/configuration/subcommands/viewTopicsSubCommand.ts
+++ apps/bot/src/plugins/configuration/subcommands/viewTopicsSubCommand.ts
@@ -28,7 +28,7 @@ export const ViewTopicsSubCommand = defineSubCommand({
 
         let components = [];
 
-        if (topicsExistInDB.length != 0) {
+        if (topicsExistInDB.length !== 0) {
             console.log('No topics found in the database');
             components = [
                 {
